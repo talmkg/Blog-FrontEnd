afa63252978da70e0711e91ff08da378938819d1
Tim Afanasiev <soupsoupchannel@gmail.com>
2023-01-10 22:17:38 +0100

    ff

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -4,27 +4,58 @@ import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
 import { useState, useEffect } from "react";
 const BlogList = (props) => {
+  // useEffect(() => {
+  //   fetchBlogs();
+  // }, []);
+
+  // if (news.isEmpty()) {
+  //   fetchBlogs();
+  // } else {
+  //   console.log(`news is not empty ${news}`);
+  // }
+  // const GetSlides = (props) => {
+  //   const [data, setData] = useState();
+
+  //   useEffect(() => {
+  //     async function getData() {
+  //       const request = fetch(API);
+  //       const response = await request;
+  //       const parsed = await response.json();
+  //       setData(parsed);
+  //     }
+
+  //     getData();
+  //     // eslint-disable-next-line react-hooks/exhaustive-deps
+  //   }, []);
+
+  //
   const [news, setNews] = useState([]);
+  // const fetchBlogs = async () => {
+  //   const response = await fetch(
+  //     "https://damiansapi-production.up.railway.app/books"
+  //   );
 
-  useEffect(() => {
-    fetchBlogs();
-  }, [news]);
-
-  const fetchBlogs = async () => {
-    try {
-      const response = await fetch(
+  //   const data = await response.json();
+  //   setNews(data);
+  //   console.log(news);
+  // };
+  // fetchBlogs();
+  React.useEffect(() => {
+    const fetching = async () => {
+      let response = await fetch(
         "https://damiansapi-production.up.railway.app/books"
       );
       if (response.ok) {
-        const data = await response.json();
-        setNews(data);
-        console.log(news);
+        const fetchedData = await response.json();
+        setNews(fetchedData);
+      } else {
+        console.log("error");
       }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    };
+    fetching();
+  }, []);
 
+  console.log(news);
   return (
     <Row>
       {posts.map((post) => (
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import BlogList from "../../components/blog/blog-list/BlogList";
 import "./styles.css";
-
+import { useState } from "react";
 const Home = (props) => {
+  const [news, setNews] = useState([]);
+
   return (
     <Container fluid="sm">
       <h1 className="blog-main-title">Welcome to the Epicode Blog!</h1>
