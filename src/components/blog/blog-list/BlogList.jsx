4279038afd85d13583141eac65fc2f23d39e1d92
Tim Afanasiev <soupsoupchannel@gmail.com>
2023-01-10 17:40:51 +0100

    idk

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -2,8 +2,27 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
-
+import { useState, useEffect } from "react";
 const BlogList = (props) => {
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    fetchBlogs();
+  });
+  const fetchBlogs = async () => {
+    try {
+      const response = await fetch(
+        "https://damiansapi-production.up.railway.app/books"
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setNews(data);
+        console.log(news);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Row>
       {posts.map((post) => (
