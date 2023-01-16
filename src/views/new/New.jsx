d00ee6e119aa7545d45be2e53e134df78f8363fa
Tim Afanasiev <soupsoupchannel@gmail.com>
2023-01-16 18:26:50 +0100

    GG

--- src/views/new/New.jsx
+++ src/views/new/New.jsx
@@ -7,8 +7,10 @@ import { newPostAction } from "../../redux/actions";
 import { addCoverAction } from "../../redux/actions";
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import Spinner from "react-bootstrap/Spinner";
 import "./styles.css";
 const NewBlogPost = (props) => {
+  let [loading, setLoading] = useState(false);
   const [poster, setPoster] = useState([]);
   const [title, setTitle] = useState("");
   const [nickname, setNickname] = useState("");
@@ -49,6 +51,7 @@ const NewBlogPost = (props) => {
     fieldToSet(value);
   };
   const onSubmitHandler = () => {
+    setLoading(true);
     const formData = new FormData();
     console.log(poster);
     formData.append("cover", poster);
@@ -80,113 +83,124 @@ const NewBlogPost = (props) => {
           .then((response) => response.json())
           .then((s) => {
             if (s) {
+              // setLoading(false);
               window.location.replace("/");
             }
           })
       );
   };
-
-  return (
-    <Container className="new-blog-container text-light">
-      <Form className="mt-3">
-        <Form.Group controlId="blog-form">
-          <Form.Label>Title</Form.Label>
-          <Form.Control
-            size="lg"
-            placeholder="Title of your blog"
-            value={title}
-            onChange={(e) => onChangeHandler(e.target.value, setTitle)}
-          />
-        </Form.Group>
-        <Row>
-          <Col>
-            <Form.Group controlId="name" className="mt-3 ">
-              <Form.Label>Name</Form.Label>
-              <Form.Control
-                size="md"
-                placeholder="Anthony Stark"
-                value={name}
-                onChange={(e) => onChangeHandler(e.target.value, setName)}
-              />
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group controlId="nickname" className="mt-3 ">
-              <Form.Label>Nickame</Form.Label>
-              <Form.Control
-                size="md"
-                placeholder="@jasonbourne"
-                value={nickname}
-                onChange={(e) => onChangeHandler(e.target.value, setNickname)}
-              />
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group controlId="time" className="mt-3">
-              <Form.Label>Read-Time</Form.Label>
-              <Form.Control
-                size="md"
-                placeholder="2"
-                value={value}
-                onChange={(e) => onChangeHandler(e.target.value, setValue)}
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-
-        <div>
-          <div>
-            <Form.Group
-              controlId="blog-content"
-              className="mt-3 p-2 rounded bg-light text-dark"
-              id="content"
-            >
-              <Form.Label>Blog Content</Form.Label>
-              <Editor
-                editorState={editorState}
-                toolbarClassName="toolbarClassName"
-                wrapperClassName="wrapperClassName"
-                editorClassName="editorClassName"
-                onEditorStateChange={setEditorState}
-              />
-            </Form.Group>
-          </div>
+  if (loading === true) {
+    return (
+      <div className="text-light" id="full-screen">
+        <div className="text-center">
+          <h3>Posting...</h3>
+          <Spinner animation="border" className="mt-2" />
         </div>
-        <div className="d-flex justify-content-start w-100 p-2">
-          <Row className="d-flex">
-            <Col xs={4}>
-              <Form.Label>Upload Your cover:</Form.Label>
+      </div>
+    );
+  } else {
+    return (
+      <Container className="new-blog-container text-light">
+        <Form className="mt-3">
+          <Form.Group controlId="blog-form">
+            <Form.Label>Title</Form.Label>
+            <Form.Control
+              size="lg"
+              placeholder="Title of your blog"
+              value={title}
+              onChange={(e) => onChangeHandler(e.target.value, setTitle)}
+            />
+          </Form.Group>
+          <Row>
+            <Col>
+              <Form.Group controlId="name" className="mt-3 ">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  size="md"
+                  placeholder="Anthony Stark"
+                  value={name}
+                  onChange={(e) => onChangeHandler(e.target.value, setName)}
+                />
+              </Form.Group>
             </Col>
-            <Col xs={8}>
-              <Form.Control
-                type="file"
-                onChange={(e) => posterChangeHandler(e)}
-                accept=".jpg, .jpeg"
-              />
+            <Col>
+              <Form.Group controlId="nickname" className="mt-3 ">
+                <Form.Label>Nickame</Form.Label>
+                <Form.Control
+                  size="md"
+                  placeholder="@jasonbourne"
+                  value={nickname}
+                  onChange={(e) => onChangeHandler(e.target.value, setNickname)}
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group controlId="time" className="mt-3">
+                <Form.Label>Read-Time</Form.Label>
+                <Form.Control
+                  size="md"
+                  placeholder="2"
+                  value={value}
+                  onChange={(e) => onChangeHandler(e.target.value, setValue)}
+                />
+              </Form.Group>
             </Col>
           </Row>
-        </div>
-        <Form.Group className="d-flex mt-3 justify-content-end">
-          <Button type="reset" size="lg" variant="outline-light">
-            Reset
-          </Button>
 
-          <Button
-            // type="submit"
-            size="lg"
-            id="submit_button"
-            variant="light"
-            style={{
-              marginLeft: "1em",
-            }}
-            onClick={onSubmitHandler}
-          >
-            Submit
-          </Button>
-        </Form.Group>
-      </Form>
-    </Container>
-  );
+          <div>
+            <div>
+              <Form.Group
+                controlId="blog-content"
+                className="mt-3 p-2 rounded bg-light text-dark"
+                id="content"
+              >
+                <Form.Label>Blog Content</Form.Label>
+                <Editor
+                  editorState={editorState}
+                  toolbarClassName="toolbarClassName"
+                  wrapperClassName="wrapperClassName"
+                  editorClassName="editorClassName"
+                  onEditorStateChange={setEditorState}
+                />
+              </Form.Group>
+            </div>
+          </div>
+          <div className="d-flex justify-content-start w-100 p-2">
+            <Row className="d-flex">
+              <Col xs={4}>
+                <Form.Label>Upload Your cover:</Form.Label>
+              </Col>
+              <Col xs={8}>
+                <Form.Control
+                  type="file"
+                  onChange={(e) => posterChangeHandler(e)}
+                  accept=".jpg, .jpeg"
+                />
+              </Col>
+            </Row>
+          </div>
+          <Form.Group className="d-flex mt-3 justify-content-end">
+            <Button type="reset" size="lg" variant="outline-light">
+              Reset
+            </Button>
+
+            <Button
+              // type="submit"
+              size="lg"
+              id="submit_button"
+              variant="light"
+              style={{
+                marginLeft: "1em",
+              }}
+              onClick={onSubmitHandler}
+            >
+              Submit
+            </Button>
+          </Form.Group>
+        </Form>
+      </Container>
+    );
+  }
 };
 
 export default NewBlogPost;
