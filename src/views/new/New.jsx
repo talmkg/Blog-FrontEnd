e1ba7d36f402e0463fb5e1c9118ab27109d2ec4a
Tim Afanasiev <soupsoupchannel@gmail.com>
2023-01-18 20:21:46 +0100

    fix

--- src/views/new/New.jsx
+++ src/views/new/New.jsx
@@ -39,7 +39,9 @@ const NewBlogPost = (props) => {
     content: `${html}`,
     author: {
       name: username,
-      avatar: user.pfp,
+      avatar:
+        user.pfp ||
+        "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
     },
     readTime: {
       value: value,
